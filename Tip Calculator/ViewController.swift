e40792d970bb763728d45c287e844019c5edbd95
Martin Sjoelin <martin@Martins-MacBook-Pro.local>
2017-02-19 18:21:23 -0800

    Initial basic Tip Calculator.

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Martin Sjoelin on 2/19/17.
+//  Copyright © 2017 Martin Sjoelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    
+    @IBAction func billUpdated2(_ sender: Any) {
+//        let tipPercentages = [0.18, 0.2, 0.25]
+//        
+//        let bill = Double(billField.text!) ?? 0
+//        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+//        let total = bill + tip
+//        
+//        tipLabel.text = String(format: "$%.2f", tip)
+//        totalLabel.text = String(format: "$%.2f", total)
+        
+        calculateTip()
+    }
+    
+    @IBAction func billUpdated(_ sender: Any) {
+        calculateTip()
+    }
+
+    @IBAction func tipUpdated(_ sender: Any) {
+        calculateTip()
+    }
+    
+    func calculateTip() {
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+}
+
